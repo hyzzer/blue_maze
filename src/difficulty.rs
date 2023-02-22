1488f73bcdb80c403bde43a2e54c680a553af1dd
hyzzer <rloucas@gmail.com>
2023-02-22 20:17:25 +0100

    FEAT (difficulty) : Handle errors

--- src/difficulty.rs
+++ src/difficulty.rs
@@ -1,9 +1,11 @@
 use std::io;
 
+#[derive(PartialEq)]
 pub enum Difficulty {
     Easy,
     Normal,
-    Hard
+    Hard,
+    Error,
 }
 
 impl Difficulty {
@@ -12,26 +14,34 @@ impl Difficulty {
             Difficulty::Easy => 16,
             Difficulty::Normal => 32,
             Difficulty::Hard => 64,
+            Difficulty::Error => 0,
         }
     }
 }
 
 pub fn get_difficulty() -> Difficulty {
-    let mut difficulty_input = String::new();
+    
+    let mut result = Difficulty::Error;
+    
+    while result == Difficulty::Error {
+        let mut difficulty_input = String::new();
+        println!("\nChoose difficulty : ");
+        println!("1. EASY");
+        println!("2. NORMAL");
+        println!("3. HARD\n");
+        
+        io::stdin()
+            .read_line(&mut difficulty_input)
+            .expect("Failed to read user input");
 
-    println!("Choose difficulty : ");
-    println!("1. EASY");
-    println!("2. NORMAL");
-    println!("3. HARD");
-    
-    io::stdin()
-        .read_line(&mut difficulty_input)
-        .expect("Failed to read user input");
-    
-    match difficulty_input.as_str() {
-        "1" => Difficulty::Easy,
-        "2" => Difficulty::Normal,
-        "3" => Difficulty::Hard,
-        _ => Difficulty::Easy,
+        println!("{}", difficulty_input);
+        
+        result = match difficulty_input.trim() {
+            "1" => Difficulty::Easy,
+            "2" => Difficulty::Normal,
+            "3" => Difficulty::Hard,
+            _ => Difficulty::Error,
+        };
     }
+    result
 }
