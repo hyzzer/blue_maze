31a425b420db685fd642c77042f0a40b4e021fe5
hyzzer <rloucas@gmail.com>
2023-02-23 02:16:07 +0100

    FEAT (board) : change initialize_boxes function to get random distribution of boxes

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 #[derive(Clone)]
 #[derive(Debug)]
 pub enum Boxes {
@@ -14,7 +16,22 @@ pub struct Board {
 
 impl Board {
     fn initalize_boxes(size: usize) -> Vec<Boxes> {
-        vec![Boxes::UP; size]
+        fn get_random_box() -> Boxes {
+            let mut rng = rand::thread_rng();
+            let random_number = rng.gen_range(0..4);
+            match random_number {
+                0 => Boxes::UP,
+                1 => Boxes::DOWN,
+                2 => Boxes::LEFT,
+                3 => Boxes::RIGHT,
+                _ => unreachable!()
+            }
+        }
+        let mut boxes: Vec<Boxes> = Vec::with_capacity(size);   
+        for _ in 0..size {
+            boxes.push(get_random_box())
+        }
+        boxes
     }
 
     pub fn new(size: usize) -> Self {
