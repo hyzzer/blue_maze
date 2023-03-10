785fad5047bdce9bb7975b37b459a3e20f4abf6c
hyzzer <rloucas@gmail.com>
2023-03-10 15:27:53 +0100

    FEAT (Player) : System setup changed for system_set to have a more pacman-like movement

--- src/player.rs
+++ src/player.rs
@@ -1,11 +1,21 @@
 use bevy::prelude::*;
-use crate::{GameTextures, WinSize, SPRITE_SCALE, PLAYER_SIZE};
+use bevy::time::FixedTimestep;
+use crate::assets::{PLAYER_SIZE, SPRITE_SCALE};
+use crate::components::{Player, Position};
+use crate::game::{SPEED};
+use crate::{GameTextures, WinSize};
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system); 
+        app
+            .add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
+            .add_system_set(
+                SystemSet::new()
+                    .with_run_criteria(FixedTimestep::step(0.10))
+                    .with_system(player_keyboard_event_system),
+            );
     }
 }
 
@@ -19,10 +29,35 @@ fn player_spawn_system(
     commands.spawn(SpriteBundle {
         texture: game_textures.player.clone(),
         transform: Transform {
-            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2.0 * SPRITE_SCALE, 2.),
+            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. * SPRITE_SCALE, 2.),
             scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
             ..default()
         },
         ..default()
+    })
+    .insert(Player)
+    .insert(Position {
+        x: 0,
+        y: 0,
     });
+}
+
+fn player_keyboard_event_system(
+    keyboard: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+    if let Ok(mut transform) = query.get_single_mut() {
+        if keyboard.pressed(KeyCode::Up) {
+            transform.translation.y += SPEED;
+        }
+        if keyboard.pressed(KeyCode::Down) {
+            transform.translation.y -= SPEED;
+        }
+        if keyboard.pressed(KeyCode::Right) {
+            transform.translation.x += SPEED;
+        }
+        if keyboard.pressed(KeyCode::Left) {
+            transform.translation.x -= SPEED;
+        }
+    }
 }
