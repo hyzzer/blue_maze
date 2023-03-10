0e9cb98f93d533e825c5dd760ccea3105e45d25e
hyzzer <rloucas@gmail.com>
2023-03-10 15:25:46 +0100

    FIX (Difficulty) : Easy difficulty changed for size 8 to size 12

--- src/difficulty.rs
+++ src/difficulty.rs
@@ -11,7 +11,7 @@ pub enum Difficulty {
 impl Difficulty {
     pub fn get_size(&self) -> usize {
         match *self {
-            Difficulty::Easy => 5,
+            Difficulty::Easy => 12,
             Difficulty::Normal => 16,
             Difficulty::Hard => 32,
             Difficulty::Error => 0,
