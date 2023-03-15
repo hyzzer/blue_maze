23de2db69a7606cfeba78c838aae0ed93c748e17
hyzzer <rloucas@gmail.com>
2023-03-15 17:05:10 +0100

    FEAT (assets) : new assets added (outlines road, walls and objects)

--- src/assets.rs
+++ src/assets.rs
@@ -16,14 +16,51 @@ impl PlayerSprite {
     }
 }
 
+// GAME TEXTURES
 pub const TILE_TOP: &str = "tile_top.png";
 pub const TILE_BOTTOM: &str = "tile_bottom.png";
-pub const TILE_WALL: &str = "tile_wall.png";
 pub const TILE_ROAD: &str = "tile_road.png";
+pub const TILE_LEFT_01: &str = "tile_left_01.png";
+pub const TILE_LEFT_02: &str = "tile_left_02.png";
+pub const TILE_RIGHT_01: &str = "tile_right_01.png";
+
+pub const TILE_BACKGROUND: &str = "tile_background.png";
+
+pub const TILE_TOP_LEFT_CORNER: &str = "tile_top_left_corner.png";
+
+pub const TILE_TOP_RIGHT_CORNER_02: &str = "tile_top_right_corner_02.png";
+
+pub const TILE_BOTTOM_LEFT_CORNER_02: &str = "tile_bottom_left_corner_02.png";
+
+pub const TILE_BOTTOM_RIGHT_CORNER: &str = "tile_bottom_right_corner.png";
+
+pub const TILE_ENTER_TOP_01: &str = "tile_enter_top_01.png";
+pub const TILE_ENTER_TOP_02: &str = "tile_enter_top_02.png";
+
+
+pub const TILE_EXIT_BOTTOM: &str = "tile_exit_bottom.png";
+pub const TILE_EXIT_BELOW: &str = "tile_exit_below.png";
+
+pub const DOOR_CLOSED: &str = "door_closed.png";
+pub const CHEST_CLOSED: &str = "chest_closed.png";
+
+
+// OUTLINE TILES
+
+pub const TILE_OUTLINE_TOP: &str = "tile_outline_top.png";
+pub const TILE_OUTLINE_TOP_LEFT: &str = "tile_outline_top_left.png";
+pub const TILE_OUTLINE_TOP_RIGHT: &str = "tile_outline_top_right.png";
+pub const TILE_OUTLINE_BOTTOM: &str = "tile_outline_bottom.png";
+pub const TILE_OUTLINE_BOTTOM_LEFT: &str = "tile_outline_bottom_left.png";
+pub const TILE_OUTLINE_BOTTOM_RIGHT: &str = "tile_outline_bottom_right.png";
+pub const TILE_OUTLINE_LEFT: &str = "tile_outline_left.png";
+pub const TILE_OUTLINE_RIGHT: &str = "tile_outline_right.png";
+
+pub const TILE_ROAD_ENTER: &str = "tile_road_enter.png";
 
 // SIZE
 pub const PLAYER_SIZE: (f32, f32) = (410., 560.);
-pub const SPRITE_SCALE: f32 = 0.08;
+pub const SPRITE_SCALE: f32 = 0.07;
 
 pub const TILE_SIZE: (f32, f32) = (48., 48.);
 pub const WALL_SIZE: (f32, f32) = (5., 48.);
--- src/main.rs
+++ src/main.rs
@@ -34,8 +34,30 @@ pub struct GameTextures {
     player: Handle<Image>,
     tile_top: Handle<Image>,
     tile_bottom: Handle<Image>,
-    tile_wall: Handle<Image>,
     tile_road: Handle<Image>,
+    tile_left_01: Handle<Image>,
+    tile_left_02: Handle<Image>,
+    tile_right_01: Handle<Image>,
+    tile_background: Handle<Image>,
+    tile_top_left_corner: Handle<Image>,
+    tile_top_right_corner_02: Handle<Image>,
+    tile_bottom_left_corner_02: Handle<Image>,
+    tile_bottom_right_corner: Handle<Image>,
+    tile_enter_top_01: Handle<Image>,
+    tile_enter_top_02: Handle<Image>,
+    tile_exit_bottom: Handle<Image>,
+    door_closed: Handle<Image>,
+    chest_closed: Handle<Image>,
+    tile_exit_below: Handle<Image>,
+    tile_outline_top: Handle<Image>,
+    tile_outline_top_left: Handle<Image>,
+    tile_outline_top_right: Handle<Image>,
+    tile_outline_bottom: Handle<Image>,
+    tile_outline_bottom_left: Handle<Image>,
+    tile_outline_bottom_right: Handle<Image>,
+    tile_outline_left: Handle<Image>,
+    tile_outline_right: Handle<Image>,
+    tile_road_enter: Handle<Image>,
 }
 
 fn setup_system(
@@ -58,15 +80,38 @@ fn setup_system(
         player: asset_server.load(assets::PlayerSprite::Golem01.get_path()),
         tile_top: asset_server.load(assets::TILE_TOP),
         tile_bottom: asset_server.load(assets::TILE_BOTTOM),
-        tile_wall: asset_server.load(assets::TILE_WALL),
         tile_road: asset_server.load(assets::TILE_ROAD),
+        tile_left_01: asset_server.load(assets::TILE_LEFT_01),
+        tile_left_02: asset_server.load(assets::TILE_LEFT_02),
+        tile_right_01: asset_server.load(assets::TILE_RIGHT_01),
+
+        tile_background: asset_server.load(assets::TILE_BACKGROUND),
+        tile_top_left_corner: asset_server.load(assets::TILE_TOP_LEFT_CORNER),
+        tile_top_right_corner_02: asset_server.load(assets::TILE_TOP_RIGHT_CORNER_02),
+        tile_bottom_left_corner_02: asset_server.load(assets::TILE_BOTTOM_LEFT_CORNER_02),
+        tile_bottom_right_corner: asset_server.load(assets::TILE_BOTTOM_RIGHT_CORNER),
+        tile_enter_top_01: asset_server.load(assets::TILE_ENTER_TOP_01),
+        tile_enter_top_02: asset_server.load(assets::TILE_ENTER_TOP_02),
+        tile_exit_bottom: asset_server.load(assets::TILE_EXIT_BOTTOM),
+        chest_closed: asset_server.load(assets::CHEST_CLOSED),
+        door_closed: asset_server.load(assets::DOOR_CLOSED),
+        tile_exit_below: asset_server.load(assets::TILE_EXIT_BELOW),
+        tile_outline_top: asset_server.load(assets::TILE_OUTLINE_TOP),
+        tile_outline_top_left: asset_server.load(assets::TILE_OUTLINE_TOP_LEFT),
+        tile_outline_top_right: asset_server.load(assets::TILE_OUTLINE_TOP_RIGHT),
+        tile_outline_bottom: asset_server.load(assets::TILE_OUTLINE_BOTTOM),
+        tile_outline_bottom_left: asset_server.load(assets::TILE_OUTLINE_BOTTOM_LEFT),
+        tile_outline_bottom_right: asset_server.load(assets::TILE_OUTLINE_BOTTOM_RIGHT),
+        tile_outline_left: asset_server.load(assets::TILE_OUTLINE_LEFT),
+        tile_outline_right: asset_server.load(assets::TILE_OUTLINE_RIGHT),
+        tile_road_enter: asset_server.load(assets::TILE_ROAD_ENTER),
+
     };
     commands.insert_resource(game_textures);
 }
 
 fn main() {
     let game = game::Game::new(difficulty::Difficulty::Easy);
-    println!("{}", game.board);
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.)))
         .insert_resource(BoardSize {
@@ -84,7 +129,7 @@ fn main() {
                 width: 1500.0,
                 height: 700.0,
                 decorations: true,
-                resizable: false,
+                // resizable: false,
               ..default()
             },
             ..default()
