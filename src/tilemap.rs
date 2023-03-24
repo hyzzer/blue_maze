634ffadb7ed60a5d96546456386fe1bdefe13c2b
hyzzer <rloucas@gmail.com>
2023-03-24 19:56:46 +0100

    REFACTOR (tilemap) : background added + tile_background removed in tilemap_spawn_system

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use crate::board::{WallStatus, Board};
 use crate::game::{BoardSize};
-use crate::{GameTextures};
+use crate::{GameTextures, WinSize};
 use crate::assets::{TILE_SIZE, WALL_SIZE};
 
 pub struct TilemapPlugin;
@@ -9,10 +9,21 @@ pub struct TilemapPlugin;
 impl Plugin for TilemapPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_startup_system_to_stage(StartupStage::PostStartup, background_spawn_system)
             .add_startup_system_to_stage(StartupStage::PostStartup, tilemap_spawn_system)
             .add_startup_system_to_stage(StartupStage::PostStartup, wallmap_spawn_system)            
             .add_startup_system_to_stage(StartupStage::PostStartup, objects_spawn_system);
     }
+}
+
+fn background_spawn_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    win_size: Res<WinSize>,
+) {
+    let scale_x = win_size.w / TILE_SIZE.0;
+    let scale_y = win_size.h / TILE_SIZE.1;
+    tile_spawn_system(&mut commands, game_textures.tile_background.clone(), Vec3::splat(0.), Some(Vec3::new(scale_x, scale_y, 1.)));
 }
 
 fn tilemap_spawn_system(
@@ -20,152 +31,103 @@ fn tilemap_spawn_system(
     game_textures: Res<GameTextures>,
     board_size: Res<BoardSize>,
 ) {
-    let start_x = - (board_size.x as f32 / 2. + 1.) * TILE_SIZE.0 - TILE_SIZE.0 / 2.;
-    let start_y = (board_size.y as f32 / 2. + 4.) * TILE_SIZE.1 - TILE_SIZE.1 / 2.;
+    let start_x = - (board_size.x as f32 / 2.) * TILE_SIZE.0 - TILE_SIZE.0 / 2.;
+    let start_y = (board_size.y as f32 / 2. + 1.) * TILE_SIZE.1 - TILE_SIZE.1 / 2.;
     let mut tile_sprite: Handle<Image>;
 
-    for row_idx in 0..board_size.y + 8 {
-        for column_idx in 0..board_size.x + 4 {
-
-            if row_idx < 3 || row_idx > board_size.y + 4 {
-                tile_sprite = game_textures.tile_background.clone();
-            }
-            else if row_idx == 3 {
-                if column_idx < 2 {
+    for row_idx in 0..board_size.y + 2 {
+        for column_idx in 0..board_size.x + 2 {
+            if row_idx == 0 {
+                if column_idx == 0 {
                     tile_sprite = game_textures.tile_background.clone();
                 }
-                else if column_idx == 2 {
+                else if column_idx == 1 {
                     tile_sprite = game_textures.tile_top_left_corner.clone();
-                }
-                else if column_idx == board_size.x + 3 {
-                    tile_sprite = game_textures.tile_top_right_corner.clone();
                 }
                 else {
                     tile_sprite = game_textures.tile_top.clone();
                 }
             }
-            else if row_idx == board_size.y + 4 {
+            else if row_idx == 1 {
                 if column_idx == 0 {
-                    tile_sprite = game_textures.tile_bottom_left_corner.clone();
-                }
-                else if column_idx == board_size.x + 1 {
-                    tile_sprite = game_textures.tile_bottom_right_corner.clone();
-                }
-                else if column_idx > board_size.x + 1 {
-                    tile_sprite = game_textures.tile_background.clone();
-                }
-                else {
-                    tile_sprite = game_textures.tile_bottom.clone();
-                }
-            }
-            else if row_idx == 4 {
-                if column_idx == 0 {
-                    tile_sprite = game_textures.tile_background.clone();
-                }
-                else if column_idx == 1 {
                     tile_sprite = game_textures.tile_left.clone();
                 }
-                else if column_idx == 2 {
+                else if column_idx == 1 {
                     tile_sprite = game_textures.tile_outline_top_left.clone();
                 }
-                else if column_idx == board_size.x + 1 {
+                else if column_idx == board_size.x {
                     tile_sprite = game_textures.tile_outline_top_right.clone();
                 }
-                else if column_idx >= board_size.x + 1 {
+                else if column_idx == board_size.x + 1 {
                     tile_sprite = game_textures.tile_road_enter.clone();
                 }
                 else {
                     tile_sprite = game_textures.tile_outline_top.clone();
                 }
             }
-            else if row_idx == board_size.y + 3 {
-                if column_idx < 2 {
+            else if row_idx == board_size.y {
+                if column_idx == 0  {
                     tile_sprite = game_textures.tile_road_enter.clone();
                 }
-                else if column_idx == 2 {
+                else if column_idx == 1 {
                     tile_sprite = game_textures.tile_outline_bottom_left.clone();
                 }
-                else if column_idx == board_size.x + 1 {
+                else if column_idx == board_size.x {
                     tile_sprite = game_textures.tile_outline_bottom_right.clone();
                 }
-                else if column_idx == board_size.x + 2 {
+                else if column_idx == board_size.x + 1 {
                     tile_sprite = game_textures.tile_right.clone();
-                }
-                else if column_idx == board_size.x + 3 {
-                    tile_sprite = game_textures.tile_background.clone();
                 }
                 else {
                     tile_sprite = game_textures.tile_outline_bottom.clone();
                 }
             }
-            else if row_idx == 5 {
-                if column_idx == 0 {
+            else if row_idx == board_size.y + 1 {
+                if column_idx == board_size.x + 1 {
                     tile_sprite = game_textures.tile_background.clone();
                 }
-                else if column_idx == 1 {
-                    tile_sprite = game_textures.tile_left.clone();
-                }
-                else if column_idx == 2 {
-                    tile_sprite = game_textures.tile_outline_left.clone();
-                }
-                else if column_idx == board_size.x + 1 {
-                    tile_sprite = game_textures.tile_outline_right.clone();
-                }                
-                else if column_idx == board_size.x + 2 {
-                    tile_sprite = game_textures.tile_exit_bottom_01.clone();
-                }
-                else if column_idx == board_size.x + 3 {
-                    tile_sprite = game_textures.tile_exit_bottom_02.clone();
+                else if column_idx == board_size.x {
+                    tile_sprite = game_textures.tile_bottom_right_corner.clone();
                 }
                 else {
-                    tile_sprite = game_textures.tile_road.clone();
-                }
-            }
-            else if row_idx == board_size.y + 2 {
-                if column_idx == 0 {
-                    tile_sprite = game_textures.tile_enter_top_02.clone();
-                }
-                else if column_idx == 1 {
-                    tile_sprite = game_textures.tile_enter_top_01.clone();
-                }
-                else if column_idx == 2 {
-                    tile_sprite = game_textures.tile_outline_left.clone();
-                }
-                else if column_idx == board_size.x + 1 {
-                    tile_sprite = game_textures.tile_outline_right.clone();
-                }                
-                else if column_idx == board_size.x + 2 {
-                    tile_sprite = game_textures.tile_right.clone();
-                }
-                else if column_idx == board_size.x + 3 {
-                    tile_sprite = game_textures.tile_background.clone();
-                }
-                else {
-                    tile_sprite = game_textures.tile_road.clone();
+                    tile_sprite = game_textures.tile_bottom.clone();
                 }
             }
             else {
-                if column_idx == 0 || column_idx == board_size.x + 3 {
-                    tile_sprite = game_textures.tile_background.clone();
+                if column_idx == 0 {
+                    if row_idx == board_size.y - 1 {
+                        tile_sprite = game_textures.tile_enter_top_01.clone();
+                    } else {
+                        tile_sprite = game_textures.tile_left.clone();
+                    }
                 }
                 else if column_idx == 1 {
-                    tile_sprite = game_textures.tile_left.clone();
-                }
-                else if column_idx == 2 {
                     tile_sprite = game_textures.tile_outline_left.clone();
                 }
-                else if column_idx == board_size.x + 1 {
+                else if column_idx == board_size.x {
                     tile_sprite = game_textures.tile_outline_right.clone();
                 }
-                else if column_idx == board_size.x + 2 {
-                    tile_sprite = game_textures.tile_right.clone();
+                else if column_idx == board_size.x + 1 {
+                    if row_idx == 2 {
+                        tile_sprite = game_textures.tile_exit_bottom_01.clone();
+                    } else {
+                        tile_sprite = game_textures.tile_right.clone();
+                    }
                 }
                 else {
                     tile_sprite = game_textures.tile_road.clone();
                 }
             }
-            tile_spawn_system(&mut commands, tile_sprite, Vec3::new(start_x + TILE_SIZE.0 * column_idx as f32, start_y - TILE_SIZE.1 * row_idx as f32, 0.), None);
+            tile_spawn_system(&mut commands, tile_sprite, Vec3::new(start_x + TILE_SIZE.0 * column_idx as f32, start_y - TILE_SIZE.1 * row_idx as f32, 1.), None);
         }
+        // CAS PARTICULIERS
+        tile_spawn_system(&mut commands, game_textures.tile_enter_top_02.clone(), Vec3::new(start_x - TILE_SIZE.0, start_y - TILE_SIZE.1 * (board_size.y as f32 - 1.), 2.), None);
+        tile_spawn_system(&mut commands, game_textures.tile_road_enter.clone(), Vec3::new(start_x - TILE_SIZE.0, start_y - TILE_SIZE.1 * board_size.y as f32, 2.), None);
+        tile_spawn_system(&mut commands, game_textures.tile_bottom_left_corner.clone(), Vec3::new(start_x - TILE_SIZE.0, start_y - TILE_SIZE.1 * (board_size.y as f32 + 1.), 2.), None);
+        
+        tile_spawn_system(&mut commands, game_textures.tile_top_right_corner.clone(), Vec3::new(start_x + TILE_SIZE.0 * (board_size.x as f32 + 2.), start_y, 2.), None);
+        tile_spawn_system(&mut commands, game_textures.tile_road_enter.clone(), Vec3::new(start_x + TILE_SIZE.0 * (board_size.x as f32 + 2.), start_y - TILE_SIZE.1, 2.), None);
+        tile_spawn_system(&mut commands, game_textures.tile_exit_bottom_02.clone(), Vec3::new(start_x + TILE_SIZE.0 * (board_size.x as f32 + 2.), start_y - TILE_SIZE.1 * 2., 2.), None);
     }
 }
 
